5ba7c22d248652433530ec48af6d7a6e0c708c77
esetiawan <esther.irawati@gmail.com>
2022-12-14 09:46:54 +0700

    14 Des

--- lib/presentation/pages/news_screen.dart
+++ lib/presentation/pages/news_screen.dart
@@ -3,38 +3,25 @@ import '../../data/model/article.dart';
 import '../../data/api/apiservice.dart';
 import '../../provider/newsprovider.dart';
 import 'package:provider/provider.dart';
-import '../bloc/news_search/news_search_bloc.dart';
 import 'newslistpage.dart';
-import 'newssearchlistpage.dart';
 
-class NewsScreenSearch extends StatefulWidget {
-  static const routeName='/article_search_list';
-  const NewsScreenSearch({Key? key}):super(key:key);
+class NewsScreen extends StatefulWidget {
+  static const routeName='/article_list';
+  const NewsScreen({Key? key}):super(key:key);
   @override
-  State<NewsScreenSearch> createState() => _NewsScreenSearchState();
+  State<NewsScreen> createState() => _NewsScreenState();
 }
 
-class _NewsScreenSearchState extends State<NewsScreenSearch> {
+class _NewsScreenState extends State<NewsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("Search Article"),),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child:
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                TextField(
-                  onChanged: (query) {
-                    context.read<NewsSearchBloc>().add(OnQueryNewsChanged(query));
-                  },
-                ),
-                SizedBox(height:16),
-                Text('Search Result'),
-                NewsSearchListPage(),
-              ],
-            ),
-        ));
+        body:
+        ChangeNotifierProvider<NewsProvider>(
+          create: (_)=>NewsProvider(apiService: ApiService(),country:'id',
+              category:'business'),
+          child: const NewsListPage(),
+        )
+    );
   }
 }
---
+++ lib/presentation/pages/news_search_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import '../../data/model/article.dart';
+import '../../data/api/apiservice.dart';
+import '../../provider/newsprovider.dart';
+import 'package:provider/provider.dart';
+import '../bloc/news_search/news_search_bloc.dart';
+import 'newslistpage.dart';
+import 'newssearchlistpage.dart';
+
+class NewsScreenSearch extends StatefulWidget {
+  static const routeName='/article_search_list';
+  const NewsScreenSearch({Key? key}):super(key:key);
+  @override
+  State<NewsScreenSearch> createState() => _NewsScreenSearchState();
+}
+
+class _NewsScreenSearchState extends State<NewsScreenSearch> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text("Search Article"),),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child:
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextField(
+                onChanged: (query) {
+                  context.read<NewsSearchBloc>().add(OnQueryNewsChanged(query));
+                },
+              ),
+              SizedBox(height:16),
+              Text('Search Result'),
+              NewsSearchListPage(),
+            ],
+          ),
+        ));
+  }
+}
