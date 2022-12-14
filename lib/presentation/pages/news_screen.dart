16d033ce04445180cc2f07888cab198f14382385
esetiawan <esther.irawati@gmail.com>
2022-12-14 09:45:47 +0700

    14 Des

--- lib/presentation/pages/news_screen.dart
+++ lib/presentation/pages/news_screen.dart
@@ -3,7 +3,9 @@ import '../../data/model/article.dart';
 import '../../data/api/apiservice.dart';
 import '../../provider/newsprovider.dart';
 import 'package:provider/provider.dart';
+import '../bloc/news_search/news_search_bloc.dart';
 import 'newslistpage.dart';
+import 'newssearchlistpage.dart';
 
 class NewsScreenSearch extends StatefulWidget {
   static const routeName='/article_search_list';
@@ -16,6 +18,23 @@ class _NewsScreenSearchState extends State<NewsScreenSearch> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: const NewsSearchListPage());
+        appBar: AppBar(title: Text("Search Article"),),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child:
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextField(
+                  onChanged: (query) {
+                    context.read<NewsSearchBloc>().add(OnQueryNewsChanged(query));
+                  },
+                ),
+                SizedBox(height:16),
+                Text('Search Result'),
+                NewsSearchListPage(),
+              ],
+            ),
+        ));
   }
 }
